7a9bfc57572a053da1d9686c96c1e6a841f2d80d
hyggenygge <ketul@hafnet.dk>
2020-10-08 13:19:05 +0200

    Display class and interface implemented

---
+++ LadeskabLibrary/Display/DisplaySimulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LadeskabLibrary.Display
+{
+    public class DisplaySimulator : IDisplay
+    {
+        public void DisplayConnectTelephone()
+        {
+            Console.WriteLine(("Tilslut din telefon."));
+        }
+
+        public void DisplayReadRfid()
+        {
+            Console.WriteLine(("Placer RFID tag mod scanner."));
+        }
+
+        public void DisplayConnectionError()
+        {
+            Console.WriteLine("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+        }
+
+        public void DisplayReserved()
+        {
+            Console.WriteLine(("Ladeskabet er optaget."));
+        }
+
+        public void DisplayRfidError()
+        {
+            Console.WriteLine("Forkert RFID tag");
+        }
+
+        public void DisplayRemoveTelephone()
+        {
+            Console.WriteLine("Tag din telefon ud af skabet og luk døren");
+        }
+
+    }
+}
---
+++ LadeskabLibrary/Display/IDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LadeskabLibrary.Display
+{
+    public interface IDisplay
+    {
+        void DisplayConnectTelephone();
+        void DisplayReadRfid();
+        void DisplayConnectionError();
+        void DisplayReserved();
+        void DisplayRfidError();
+        void DisplayRemoveTelephone();
+
+    }
+}
