ff00d2d97b555e1bfc2ad36596f17ea4a939cf1f
hyggenygge <ketul@hafnet.dk>
2020-10-21 12:25:56 +0200

    Added DisplaySimulatorTest

---
+++ Ladeskab.Unit.Test/DisplaySimulatorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using LadeskabLibrary;
+using LadeskabLibrary.Display;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture]
+    public class DisplaySimulatorTest
+    {
+        private StationControl _uut;
+        private IDisplay _display;
+
+        [SetUp]
+        public void Setup()
+        {
+            _display = Subsitute.For<IDisplay>();
+            _uut = new StationControl();
+        }
+
+        [Test]
+        public void DoorOpened_EventFired_DisplayConnectTelephone()
+        {
+            //_display.DisplayConnectTelephone() +=
+        }
+
+    }
+}
