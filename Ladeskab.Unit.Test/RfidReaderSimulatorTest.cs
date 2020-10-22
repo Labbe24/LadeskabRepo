5ccdada39e488d0accc3981b84b64e0b88d5e3db
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-10-22 11:01:38 +0200

    Added RfidReaderSimulatorTest and implemented first test

---
+++ Ladeskab.Unit.Test/RfidReaderSimulatorTest.cs
@@ -0,0 +1,32 @@
+﻿using LadeskabLibrary.Events;
+using LadeskabLibrary.RFID;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture]
+    public class RfidReaderSimulatorTest
+    {
+        private RfidReaderSimulator _uut;
+        private RFIDDetectedEventArgs _recievedRfidDetectedEventArgs;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _recievedRfidDetectedEventArgs = null;
+
+            _uut = new RfidReaderSimulator();
+            _uut.RFIDDetectedEvent += (o, args) =>
+            {
+                _recievedRfidDetectedEventArgs = args;
+            };
+        }
+
+        [Test]
+        public void OnRfidRead_OnRfidReadCalled_EventFired()
+        {
+            _uut.OnRfidRead(1);
+            Assert.That(_recievedRfidDetectedEventArgs, Is.Not.Null);
+        }
+    }
+}
